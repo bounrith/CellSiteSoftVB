be501f6012b414838c1f7fbf6f03a02686b645e3
tinooo <tinooo@e36cb13b-30be-48ae-ae61-9738efc03bd8>
2011-05-25 08:49:28 +0000

    Add ThumbnailView to destination folder. Refresh when file is copy to destination folder.

--- CellSiteSoft VB 2010/CellSiteSoft VB 2010.Designer.vb
+++ CellSiteSoft VB 2010/CellSiteSoft VB 2010.Designer.vb
@@ -25,7 +25,7 @@ Partial Class CellSiteSoftMain
         Me.components = New System.ComponentModel.Container()
         Me.DirectoryTreeView = New System.Windows.Forms.TreeView()
         Me.ComboBox_Drives = New System.Windows.Forms.ComboBox()
-        Me.imageList = New System.Windows.Forms.ImageList(Me.components)
+        Me.imageList1 = New System.Windows.Forms.ImageList(Me.components)
         Me.lstFileNames = New System.Windows.Forms.ListBox()
         Me.cmdCategory = New System.Windows.Forms.ComboBox()
         Me.txtSiteName = New System.Windows.Forms.TextBox()
@@ -37,15 +37,16 @@ Partial Class CellSiteSoftMain
         Me.DestinationFolder = New System.Windows.Forms.Button()
         Me.txtDestinationFolder = New System.Windows.Forms.TextBox()
         Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
-        Me.lstFilesList2 = New System.Windows.Forms.ListBox()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
         Me.COPY = New System.Windows.Forms.Button()
         Me.ListView1 = New System.Windows.Forms.ListView()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.ListView2 = New System.Windows.Forms.ListView()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
+        Me.ImageList2 = New System.Windows.Forms.ImageList(Me.components)
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
         Me.GroupBox3.SuspendLayout()
@@ -53,64 +54,72 @@ Partial Class CellSiteSoftMain
         '
         'DirectoryTreeView
         '
-        Me.DirectoryTreeView.Location = New System.Drawing.Point(14, 50)
+        Me.DirectoryTreeView.Location = New System.Drawing.Point(19, 62)
+        Me.DirectoryTreeView.Margin = New System.Windows.Forms.Padding(4)
         Me.DirectoryTreeView.Name = "DirectoryTreeView"
-        Me.DirectoryTreeView.Size = New System.Drawing.Size(210, 520)
+        Me.DirectoryTreeView.Size = New System.Drawing.Size(279, 639)
         Me.DirectoryTreeView.TabIndex = 0
         '
         'ComboBox_Drives
         '
         Me.ComboBox_Drives.FormattingEnabled = True
-        Me.ComboBox_Drives.Location = New System.Drawing.Point(14, 26)
+        Me.ComboBox_Drives.Location = New System.Drawing.Point(19, 32)
+        Me.ComboBox_Drives.Margin = New System.Windows.Forms.Padding(4)
         Me.ComboBox_Drives.Name = "ComboBox_Drives"
-        Me.ComboBox_Drives.Size = New System.Drawing.Size(210, 21)
+        Me.ComboBox_Drives.Size = New System.Drawing.Size(279, 24)
         Me.ComboBox_Drives.TabIndex = 1
         '
-        'imageList
+        'imageList1
         '
-        Me.imageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth16Bit
-        Me.imageList.ImageSize = New System.Drawing.Size(32, 32)
-        Me.imageList.TransparentColor = System.Drawing.Color.Transparent
+        Me.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth16Bit
+        Me.imageList1.ImageSize = New System.Drawing.Size(32, 32)
+        Me.imageList1.TransparentColor = System.Drawing.Color.Transparent
         '
         'lstFileNames
         '
         Me.lstFileNames.FormattingEnabled = True
-        Me.lstFileNames.Location = New System.Drawing.Point(18, 72)
+        Me.lstFileNames.ItemHeight = 16
+        Me.lstFileNames.Location = New System.Drawing.Point(24, 89)
+        Me.lstFileNames.Margin = New System.Windows.Forms.Padding(4)
         Me.lstFileNames.Name = "lstFileNames"
-        Me.lstFileNames.Size = New System.Drawing.Size(211, 498)
+        Me.lstFileNames.Size = New System.Drawing.Size(280, 612)
         Me.lstFileNames.TabIndex = 10
         '
         'cmdCategory
         '
         Me.cmdCategory.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.cmdCategory.FormattingEnabled = True
-        Me.cmdCategory.Location = New System.Drawing.Point(18, 27)
+        Me.cmdCategory.Location = New System.Drawing.Point(24, 33)
+        Me.cmdCategory.Margin = New System.Windows.Forms.Padding(4)
         Me.cmdCategory.Name = "cmdCategory"
-        Me.cmdCategory.Size = New System.Drawing.Size(211, 28)
+        Me.cmdCategory.Size = New System.Drawing.Size(280, 32)
         Me.cmdCategory.TabIndex = 11
         Me.cmdCategory.Text = "Select Category"
         '
         'txtSiteName
         '
-        Me.txtSiteName.Location = New System.Drawing.Point(234, 62)
+        Me.txtSiteName.Location = New System.Drawing.Point(312, 76)
+        Me.txtSiteName.Margin = New System.Windows.Forms.Padding(4)
         Me.txtSiteName.Name = "txtSiteName"
-        Me.txtSiteName.Size = New System.Drawing.Size(164, 20)
+        Me.txtSiteName.Size = New System.Drawing.Size(217, 22)
         Me.txtSiteName.TabIndex = 12
         '
         'txtSiteID
         '
-        Me.txtSiteID.Location = New System.Drawing.Point(503, 62)
+        Me.txtSiteID.Location = New System.Drawing.Point(671, 76)
+        Me.txtSiteID.Margin = New System.Windows.Forms.Padding(4)
         Me.txtSiteID.Name = "txtSiteID"
-        Me.txtSiteID.Size = New System.Drawing.Size(118, 20)
+        Me.txtSiteID.Size = New System.Drawing.Size(156, 22)
         Me.txtSiteID.TabIndex = 13
         '
         'lblSiteName
         '
         Me.lblSiteName.AutoSize = True
         Me.lblSiteName.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblSiteName.Location = New System.Drawing.Point(152, 62)
+        Me.lblSiteName.Location = New System.Drawing.Point(203, 76)
+        Me.lblSiteName.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblSiteName.Name = "lblSiteName"
-        Me.lblSiteName.Size = New System.Drawing.Size(76, 20)
+        Me.lblSiteName.Size = New System.Drawing.Size(93, 24)
         Me.lblSiteName.TabIndex = 15
         Me.lblSiteName.Text = "Site Name:"
         '
@@ -118,9 +127,10 @@ Partial Class CellSiteSoftMain
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(443, 62)
+        Me.Label2.Location = New System.Drawing.Point(591, 76)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(54, 20)
+        Me.Label2.Size = New System.Drawing.Size(65, 24)
         Me.Label2.TabIndex = 16
         Me.Label2.Text = "Site ID:"
         '
@@ -129,45 +139,41 @@ Partial Class CellSiteSoftMain
         Me.cmdFileSelect.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
         Me.cmdFileSelect.Font = New System.Drawing.Font("Arial Narrow", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.cmdFileSelect.ForeColor = System.Drawing.Color.Black
-        Me.cmdFileSelect.Location = New System.Drawing.Point(667, 58)
+        Me.cmdFileSelect.Location = New System.Drawing.Point(889, 71)
+        Me.cmdFileSelect.Margin = New System.Windows.Forms.Padding(4)
         Me.cmdFileSelect.Name = "cmdFileSelect"
-        Me.cmdFileSelect.Size = New System.Drawing.Size(185, 25)
+        Me.cmdFileSelect.Size = New System.Drawing.Size(247, 31)
         Me.cmdFileSelect.TabIndex = 18
         Me.cmdFileSelect.Text = "Select Check List File ..."
         Me.cmdFileSelect.UseVisualStyleBackColor = False
         '
         'CheckListFile
         '
-        Me.CheckListFile.Location = New System.Drawing.Point(858, 62)
+        Me.CheckListFile.Location = New System.Drawing.Point(1144, 76)
+        Me.CheckListFile.Margin = New System.Windows.Forms.Padding(4)
         Me.CheckListFile.Name = "CheckListFile"
-        Me.CheckListFile.Size = New System.Drawing.Size(284, 20)
+        Me.CheckListFile.Size = New System.Drawing.Size(377, 22)
         Me.CheckListFile.TabIndex = 19
         '
         'DestinationFolder
         '
         Me.DestinationFolder.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
         Me.DestinationFolder.Font = New System.Drawing.Font("Arial Narrow", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.DestinationFolder.Location = New System.Drawing.Point(11, 30)
+        Me.DestinationFolder.Location = New System.Drawing.Point(15, 37)
+        Me.DestinationFolder.Margin = New System.Windows.Forms.Padding(4)
         Me.DestinationFolder.Name = "DestinationFolder"
-        Me.DestinationFolder.Size = New System.Drawing.Size(254, 25)
+        Me.DestinationFolder.Size = New System.Drawing.Size(339, 31)
         Me.DestinationFolder.TabIndex = 20
         Me.DestinationFolder.Text = "Select COPY To Folder ..."
         Me.DestinationFolder.UseVisualStyleBackColor = False
         '
         'txtDestinationFolder
         '
-        Me.txtDestinationFolder.Location = New System.Drawing.Point(11, 73)
+        Me.txtDestinationFolder.Location = New System.Drawing.Point(15, 90)
+        Me.txtDestinationFolder.Margin = New System.Windows.Forms.Padding(4)
         Me.txtDestinationFolder.Name = "txtDestinationFolder"
-        Me.txtDestinationFolder.Size = New System.Drawing.Size(254, 20)
+        Me.txtDestinationFolder.Size = New System.Drawing.Size(337, 22)
         Me.txtDestinationFolder.TabIndex = 21
-        '
-        'lstFilesList2
-        '
-        Me.lstFilesList2.FormattingEnabled = True
-        Me.lstFilesList2.Location = New System.Drawing.Point(11, 99)
-        Me.lstFilesList2.Name = "lstFilesList2"
-        Me.lstFilesList2.Size = New System.Drawing.Size(254, 433)
-        Me.lstFilesList2.TabIndex = 22
         '
         'OpenFileDialog1
         '
@@ -177,19 +183,20 @@ Partial Class CellSiteSoftMain
         '
         Me.COPY.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
         Me.COPY.Font = New System.Drawing.Font("Arial Narrow", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.COPY.Location = New System.Drawing.Point(30, 538)
+        Me.COPY.Location = New System.Drawing.Point(40, 662)
+        Me.COPY.Margin = New System.Windows.Forms.Padding(4)
         Me.COPY.Name = "COPY"
-        Me.COPY.Size = New System.Drawing.Size(211, 32)
+        Me.COPY.Size = New System.Drawing.Size(281, 39)
         Me.COPY.TabIndex = 23
         Me.COPY.Text = "COPY"
         Me.COPY.UseVisualStyleBackColor = False
         '
         'ListView1
         '
-        Me.ListView1.Location = New System.Drawing.Point(228, 26)
-        Me.ListView1.Margin = New System.Windows.Forms.Padding(2)
+        Me.ListView1.Location = New System.Drawing.Point(304, 32)
+        Me.ListView1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.ListView1.Name = "ListView1"
-        Me.ListView1.Size = New System.Drawing.Size(389, 544)
+        Me.ListView1.Size = New System.Drawing.Size(517, 669)
         Me.ListView1.TabIndex = 24
         Me.ListView1.UseCompatibleStateImageBehavior = False
         '
@@ -197,9 +204,11 @@ Partial Class CellSiteSoftMain
         '
         Me.GroupBox1.Controls.Add(Me.cmdCategory)
         Me.GroupBox1.Controls.Add(Me.lstFileNames)
-        Me.GroupBox1.Location = New System.Drawing.Point(649, 110)
+        Me.GroupBox1.Location = New System.Drawing.Point(865, 135)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(242, 592)
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4)
+        Me.GroupBox1.Size = New System.Drawing.Size(323, 729)
         Me.GroupBox1.TabIndex = 26
         Me.GroupBox1.TabStop = False
         '
@@ -208,49 +217,69 @@ Partial Class CellSiteSoftMain
         Me.GroupBox2.Controls.Add(Me.ListView1)
         Me.GroupBox2.Controls.Add(Me.ComboBox_Drives)
         Me.GroupBox2.Controls.Add(Me.DirectoryTreeView)
-        Me.GroupBox2.Location = New System.Drawing.Point(4, 110)
+        Me.GroupBox2.Location = New System.Drawing.Point(5, 135)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(4)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(633, 592)
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(4)
+        Me.GroupBox2.Size = New System.Drawing.Size(844, 729)
         Me.GroupBox2.TabIndex = 27
         Me.GroupBox2.TabStop = False
         '
         'GroupBox3
         '
+        Me.GroupBox3.Controls.Add(Me.ListView2)
         Me.GroupBox3.Controls.Add(Me.COPY)
-        Me.GroupBox3.Controls.Add(Me.lstFilesList2)
         Me.GroupBox3.Controls.Add(Me.txtDestinationFolder)
         Me.GroupBox3.Controls.Add(Me.DestinationFolder)
-        Me.GroupBox3.Location = New System.Drawing.Point(901, 110)
+        Me.GroupBox3.Location = New System.Drawing.Point(1201, 135)
+        Me.GroupBox3.Margin = New System.Windows.Forms.Padding(4)
         Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Size = New System.Drawing.Size(276, 592)
+        Me.GroupBox3.Padding = New System.Windows.Forms.Padding(4)
+        Me.GroupBox3.Size = New System.Drawing.Size(368, 729)
         Me.GroupBox3.TabIndex = 28
         Me.GroupBox3.TabStop = False
+        '
+        'ListView2
+        '
+        Me.ListView2.Location = New System.Drawing.Point(15, 119)
+        Me.ListView2.Name = "ListView2"
+        Me.ListView2.Size = New System.Drawing.Size(337, 535)
+        Me.ListView2.TabIndex = 31
+        Me.ListView2.UseCompatibleStateImageBehavior = False
         '
         'Label3
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(439, 4)
+        Me.Label3.Location = New System.Drawing.Point(585, 5)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(305, 29)
+        Me.Label3.Size = New System.Drawing.Size(368, 36)
         Me.Label3.TabIndex = 29
         Me.Label3.Text = "Field Photo Management"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(489, 713)
+        Me.Label4.Location = New System.Drawing.Point(652, 878)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(205, 13)
+        Me.Label4.Size = New System.Drawing.Size(271, 17)
         Me.Label4.TabIndex = 30
         Me.Label4.Text = "© 2011 TRB Software. All rights reserved."
         '
+        'ImageList2
+        '
+        Me.ImageList2.ColorDepth = System.Windows.Forms.ColorDepth.Depth16Bit
+        Me.ImageList2.ImageSize = New System.Drawing.Size(32, 32)
+        Me.ImageList2.TransparentColor = System.Drawing.Color.Transparent
+        '
         'CellSiteSoftMain
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange
-        Me.ClientSize = New System.Drawing.Size(1182, 735)
+        Me.ClientSize = New System.Drawing.Size(1576, 905)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.GroupBox3)
@@ -262,6 +291,7 @@ Partial Class CellSiteSoftMain
         Me.Controls.Add(Me.lblSiteName)
         Me.Controls.Add(Me.txtSiteID)
         Me.Controls.Add(Me.txtSiteName)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "CellSiteSoftMain"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "TRB Software - Field Photo Management v0.1"
@@ -286,9 +316,8 @@ Partial Class CellSiteSoftMain
     Friend WithEvents DestinationFolder As System.Windows.Forms.Button
     Friend WithEvents txtDestinationFolder As System.Windows.Forms.TextBox
     Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
-    Friend WithEvents lstFilesList2 As System.Windows.Forms.ListBox
     Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
-    Public WithEvents imageList As System.Windows.Forms.ImageList
+    Public WithEvents imageList1 As System.Windows.Forms.ImageList
     Friend WithEvents COPY As System.Windows.Forms.Button
     Friend WithEvents ListView1 As System.Windows.Forms.ListView
     Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
@@ -296,5 +325,7 @@ Partial Class CellSiteSoftMain
     Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents Label4 As System.Windows.Forms.Label
+    Public WithEvents ImageList2 As System.Windows.Forms.ImageList
+    Friend WithEvents ListView2 As System.Windows.Forms.ListView
 
 End Class
